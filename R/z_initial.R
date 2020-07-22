6cc5e0e5ac9f9e0675d622a7b1b249565b4a1f57
lilleoel <oel@oelfam.com>
2020-07-22 14:00:37 +0200

    wokring on z_block()

--- R/z_initial.R
+++ R/z_initial.R
@@ -34,28 +34,29 @@ z_deleter <- function(df,del){
 }
 
 #Define blocks
+#Change function to act for every variable, subsequent merging - NEXT ACTION
 z_blocks <- function(df,freq,blocksize,blockmin,by_type){
 
+   #Add blocks
    df <- within(df,block <- ave(n,period,FUN = function(x) ceiling((x-min(x)+1)/(blocksize*freq))))
 
+   #Create df_block
+   df_block <- aggregate(df[,1],by=list(df$block,df$period),length)
+   colnames(df_block) <- c("block","period","length")
+   for(i in by_type){
+      df_block <- cbind(df_block,na.omit(c(by(df$val1, list(df$block,df$period), eval(parse(text = i))))))
+      df_block <- cbind(df_block,na.omit(c(by(df$val2, list(df$block,df$period), eval(parse(text = i))))))
+      colnames(df_block)[c((ncol(df_block)-1):(ncol(df_block)))] <- c(paste0("val1_",i), paste0("val2_",i))
+   }
+
+
+   #Remove blocks which does not fullfill quality control
    if(!is.null(blockmin)){
       df <- within(df,del <- ave(n,period,block, FUN = function(x)
          (length(x)<(freq*blocksize*blockmin))*1))
       del <- length(unique(df$block[df$del == 1]))
       df <- df[df$del != "1",-c(ncol(df))]
    }
-
-   df_block <- NULL
-   for(i in by_type){
-      temp <- aggregate(df[,c("val1","val2")],by=list(df$period,df$block),i)
-      colnames(temp) <- c("period","block",paste0("val1_",i),paste0("val2_",i))
-      if(!is.null(df_block)) df_block <- merge(df_block,temp,by=c("period","block"))
-      if(is.null(df_block)) df_block <- temp
-   }
-
-   #this should be faster.
-   #missing <- creation of dataframe with unique blocks, i think it exists in the cor function
-   temp <- by(df$val1, list(df$block,df$period), mean,simplify=TRUE)
 
 
 
