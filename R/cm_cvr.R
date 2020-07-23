6a1f5737370049190084e543eee5e772ad6706cc
lilleoel <oel@oelfam.com>
2020-07-23 22:18:13 +0200

    notes - calculate CO

--- R/cm_cvr.R
+++ R/cm_cvr.R
@@ -1,4 +1,5 @@
-#Cvr formel - https ://europepmc.org/ article / pmc /pmc6054990
+#Cvr formel - https://europepmc.org/article/pmc/pmc6054990
+#CVR = mean BP/mean MCAv
 
 cm_cvr <- function() {
    print("Hello, world!")
--- R/cm_tfa.R
+++ R/cm_tfa.R
@@ -1,6 +1,7 @@
 #I forhold til TFA skal det vel udspecificeres som gain, phase og coherence
 #Tfa - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4821028/#!po=0.581395
 #   https://journals.sagepub.com/doi/full/10.1038/jcbfm.2008.13
+# R script https://elucimed.com/ensemble-r/extract/pf-transfer-function/
 
 cm_tfa <- function() {
    print("Hello, world!")
